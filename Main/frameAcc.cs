c4477423aa4a1f451d4b91d0d1038a4f9ced8ce9
Hasonako <andrey-kotov04@yandex.ru>
2024-12-16 13:52:29 +0300

    Добавьте файлы проекта.

---
+++ Main/frameAcc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace gaz.Main
+{
+    internal class frameAcc
+    {
+        public static Frame frmObj;
+    }
+}
